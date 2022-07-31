6f80a5027669f2e94389d3dab33c204b1499824a
Liu Haibin <liu.haibin@gmail.com>
2022-07-31 23:09:10 +0800

    update 9

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   console.log("hello world")
   console.log("hello world 1")
   console.log("hello world 2")
+  console.log("hello world 3")
   return (
     <div className="App">
       <header className="App-header">
